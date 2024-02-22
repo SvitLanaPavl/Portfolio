6a9b83d56617d13121f4bfe9d691d819870e0100
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-02-21 18:34:54 -0600

    3-d computer component added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,9 +7,22 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
-      <hemisphereLight intesity={0.15} groundColor="black"/>
-      <pointLight intensity={1} object={computer.scene}/>
-      <primitive />
+      <hemisphereLight intesity={0.15}
+      groundColor="black"/>
+      <pointLight intensity={1} />
+      <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      />
+      <primitive 
+      object={computer.scene}
+      scale={0.75}
+      position={[0, -3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
