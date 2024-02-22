b6b688a1dd991ef54a3fcff8fd3a392ba3783e6b
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-02-21 23:00:45 -0600

    fixed earth camera values, animation on stars added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,10 +15,49 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
   const handleSubmit = (e) => {
-    
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "JavaScript Mastery",
+          from_email: form.email,
+          to_email: "sujata@jsmastery.pro",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
   };
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse gap-10 overflow-hidden">
@@ -72,7 +111,7 @@ const Contact = () => {
       </motion.div>
       <motion.div
       variants={slideIn('right', 'tween', 0.2, 1)}
-      classNAme="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
       </motion.div>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -19,12 +19,13 @@ const EarthCanvas = () => {
   <Canvas
   shadows
   frameloop='demand'
+  dpr={[1, 2]}
   gL={{preserveDrawingBuffer: true}}
   camera={{ 
-    fovL: 45,
+    fov: 45,
     near: 0.1,
     far: 200,
-    position: [-4, 3, 6]
+    position: [-4, 3, 6],
   }}
   >
     <Suspense fallback={<CanvasLoader />}>
